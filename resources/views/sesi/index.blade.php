73f97e66fcb7fbc0bbf0e9c804b6bdb42387a96e
rayy73 <r4yy73@gmail.com>
2025-10-01 14:52:09 +0700

    Update SessionController dan perbaikan route login

--- resources/views/sesi/index.blade.php
+++ resources/views/sesi/index.blade.php
@@ -12,13 +12,14 @@
     <title>Sisfo Kepegawaian</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+   <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="{{asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
+
 
 </head>
 
@@ -35,29 +36,25 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                           
+                            <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Selamat Datang!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Selamat Datang</h1>
                                     </div>
-                                    <form class="users" action="/sesi/login" method="POST">
-                                        @csrf
-
+                                    <form class="user" action="/sesi/login" method="POST">
                                         <div class="form-group">
                                             <input type="email" class="form-control form-control-user"
-                                                name="email" value0="{{Session::get('email')}}"
-                                                placeholder="Masukan Email Anda">
-                                        </div>
+                                               name="email" value="{{ Session::get('email') }}" 
+                                                placeholder="Masukkan Username / Email">
+                                        </div>  
                                         <div class="form-group">
-                                            <input type="password" name="Password" class="form-control form-control-user"
-                                                 placeholder="Masukan Password Anda">
+                                            <input type="password" name="password" class="form-control form-control-user" 
+                                                placeholder="Masukkan Password Anda">
+                                        </div>     
                                         </div>
-                                        <div class="form-group">
-                                            
-                                        </div>
-                                        <button type="Submit" class="btn btn-primary btn-user btn-block">Login</button>
-
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
+                                        
                                         
                                     </form>
                                     <hr>
@@ -75,7 +72,7 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+     <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
     <script src=" {{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
     <!-- Core plugin JavaScript-->
